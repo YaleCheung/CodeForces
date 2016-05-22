2751d44075ec819b7695eacde613f33f526d3727
YaleCheung <zybjtu@gmail.com>
2016-05-22 15:01:41 +0000

    a team not complete

---
+++ a_team.cc
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a_team.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/22/16 15:00:42
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
+
