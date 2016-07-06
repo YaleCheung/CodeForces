846749a352293959c7a7d6c915893439072a8ecd
YaleCheung <zybjtu@gmail.com>
2016-07-06 08:21:00 +0000

    add b_fedorNewGame.cc

---
+++ b_fedorNewGame.cc
@@ -0,0 +1,48 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  b_fedorNewGame.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/06/16 07:56:00
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> container;
+    for(int i = 0; i < m + 1; ++ i) {
+        int val;
+	cin >> val;
+	container.push_back(val);
+    }
+    int fedor = container.back();
+    int ret = 0;
+    for(auto i = 0; i < m; ++ i) {
+        int start = 1;
+	int diff = 0;
+	for(int j = 0; j < 31; ++ j) {
+            if((start & container[i]) != (start & fedor))
+                ++ diff;
+	    if(diff > k)
+                break;
+	    start <<= 1;
+	}
+	if (diff <= k)
+            ++ ret;
+    }
+    cout << ret << endl;
+    return 0;
+}
