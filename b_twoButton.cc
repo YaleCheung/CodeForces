453db34603cc3e2b374f54a5661fde8931de3ea4
YaleCheung <zybjtu@gmail.com>
2016-10-29 22:37:23 +0800

    sth. is wrong

--- b_twoButton.cc
+++ b_twoButton.cc
@@ -1,70 +1,19 @@
-/*
- * =====================================================================================
- *
- *       Filename:  b_twoButton.cc
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  10/19/2016 11:08:27
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
-
 #include <iostream>
-#include <assert.h>
-#include <queue>
 using std::cout;
-using std::cin;
 using std::endl;
-using std::queue;
-
-// find the items; bfs;
-void two_buttons(int m, int n) {
-    queue<int> num_queue;
-    num_queue.push(n);
-    int level = 0;
-    bool find = false;
-    while(!find) {
-        ++ level;
-        cout << "level "  << level << endl;
-        int last_line = num_queue.size();
-        for(int i = 0; i < last_line; ++ i) {
-            // process odd number;
-            auto num = num_queue.front();
-            int left = num << 1;
-            cout << left << ' ' << endl;
-            if (left == m) {
-                cout << "result " << level << endl;
-                find = true; 
-                break;
-            }
-            num_queue.push(left);
-
-            int right = -- num;
-            cout << right << ' ' << endl;
-            if (right == m) {
-                cout << "result " << level << endl;
-                find = true;
-                break;
-            }
-            num_queue.push(right);
-            
-            // delete the node;
-            num_queue.pop();
-        }
-        if (find)
-            break;
+//move
+class A {
+public:
+    A () {
+        cout << "normal constructor" << endl;
     }
-}
-int main(int argc, char* argv[]) {
-    int n, m;
-    cin >> n >> m;
-    two_buttons(m ,n);
+    A(A&& a) {
+        cout << "using move constructor" << endl;
+    }
+};
+
+int main() {
+    A a;
+    A b(std::move(a));
     return 0;
 }
