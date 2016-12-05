63a1d147c732ae43b9d0178fb3045604d3b48cb4
YaleCheung <zybjtu@gmail.com>
2016-12-05 15:35:47 +0800

    add a_alensSchedules.cc

--- a_alenasSchedule.cc
+++ a_alenasSchedule.cc
@@ -18,6 +18,28 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-
+int main(int argc, char* argv[]) {
+    int num;
+    cin >> num;
+    int number;
+    bool started = false;
+    int ret = 0;
+    int pre_zeros = 0;
+    for(int i = 0; i < num; ++ i) {
+        cin >> number;
+        if (1 == number) {
+            ++ ret;
+            if (started == false)
+                started = true;
+            if (pre_zeros == 1)
+                ++ ret;
+            pre_zeros = 0;
+        } else if (0 == number){
+            if (started == true) {
+                ++ pre_zeros;
+            }
+        }
+    }
+    cout << ret << '\n';
+    return 0;
 }
