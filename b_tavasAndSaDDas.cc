fedaa178c217ffde09c4e9d6470b8aec48b82104
YaleCheung <zybjtu@gmail.com>
2016-12-18 22:41:05 +0800

    add b_tavasAndSaDDas.cc

--- b_tavasAndSaDDas.cc
+++ b_tavasAndSaDDas.cc
@@ -17,6 +17,7 @@
  */
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 auto solution(string&& str) -> void {
@@ -28,8 +29,20 @@ auto solution(string&& str) -> void {
     for(int i = 0; i < size; ++ i) {
         ret += static_cast<int>(pow(2, i));
     }
-    if (str.at(0) == '7') {
-        ret += static_cast<int>(pow(2, size - 1));
+    // find the first '7' and calculate the variance 
+    size_t pos = 0;
+    while(pos < size && str.find('7', pos) != string::npos) {
+        int left = size - pos - 1;
+        if (left >= 0)
+            ret += static_cast<int>(pow(2, left));
+        ++ pos;
     }
-    cout << ret;
+    cout << ret << endl;
+}
+
+int main(int argc, char * argv[]) {
+    string input;
+    cin >> input;
+    solution(std::move(input));
+    return 0;
 }
