fbb829f27c18e1d5542403dcbc8e1fb60389f583
YaleCheung <zybjtu@gmail.com>
2016-12-19 18:41:34 +0800

    not sure

---
+++ a_party.cc
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a_party.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/19/2016 18:04:45
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+auto solution(vector<int> input) -> void { 
+    int ret = input.size();
+    
+}
+
+int main(int argc, char* argv[]) {
+
+}
