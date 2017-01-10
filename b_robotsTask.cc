46742909edd0b102ea9ba8c527b0049ebfc69b3c
YaleCheung <zybjtu@gmail.com>
2017-01-10 19:54:54 +0800

    fix b_robotsTask.cc

--- b_robotsTask.cc
+++ b_robotsTask.cc
@@ -12,18 +12,63 @@ auto generalNext(BidIt iter, Direction direction = RIGHT) -> BidIt {
     else
         return std::prev(iter);
 }
+template<typename BidIt>
+auto switchDirection(BidIt iter, list<int>& l, Direction direction = RIGHT) -> bool {
+    if (RIGHT == direction) {
+        if (iter == l.end() && !l.empty()) {
+            direction = LEFT; 
+            return true;
+        }
+        return false;
+    } else {
+        if (iter == l.rend().base() && !l.empty()) {
+            direction = RIGHT;
+            return true;
+        }
+        return false;
+    }
+}
+
+template<typename T>
+auto begin(list<T>& l, Direction direction = RIGHT) -> decltype(l.begin()) {
+    if (RIGHT == direction) 
+        return l.begin();
+    else 
+        return l.rbegin().base();
+}
+
+template<typename T>
+auto end(list<T>& l, Direction direction = RIGHT) -> decltype(l.end()){
+    if (RIGHT == direction) 
+        return l.end();
+    else 
+        return l.rend().base();
+}
 
 auto solution (list<int> input, int count) -> int {
     int ret= 0;
     int cur_info = 0;
     Direction direction = RIGHT;    
 
-    auto iter = input.begin();
     while(cur_info != count) {
-        if(*iter <= cur_info) {
-            input.erase(iter);
+        auto iter = begin(input, direction);
+        auto end_iter = end(input, direction);
+        while(iter != end_iter) {
+            if(*iter <= cur_info) {
+                iter = input.erase(iter);
+                ++ cur_info;
+            } else 
+                iter = generalNext(iter, direction);
         }
-        
+        if (switchDirection(iter, input, direction)) {
+            if (RIGHT == direction)
+                direction = LEFT;
+            else
+                direction = RIGHT;
+            ++ ret;
+        }
+    }
+    return ret;
 }
 
 int main(int argc, char* argv[]) {
