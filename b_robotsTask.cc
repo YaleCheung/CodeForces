1d7b055b7ebf1ec121c86ea950d9900b4fc03388
YaleCheung <zybjtu@gmail.com>
2017-01-10 19:04:09 +0800

    modify b_robotsTask.cc, but decltype doesn't work

--- b_robotsTask.cc
+++ b_robotsTask.cc
@@ -6,27 +6,65 @@ using namespace std;
 enum Direction {LEFT, RIGHT};
 
 template<typename BidIt>
-auto general_next(BidIt iter, Direction direction = RIGHT) -> BidIt {
+auto generalNext(BidIt iter, Direction direction = RIGHT) -> BidIt {
     if(RIGHT == direction)
         return std::next(iter);
     else
         return std::prev(iter);
+}
+template<typename BidIt>
+auto switchDirection(BidIt iter, list<int>& l, Direction direction = RIGHT) -> bool {
+    if (RIGHT == direction) {
+        if (iter == l.end() && !l.empty()) {
+            direction = LEFT; 
+            return true;
+        }
+        return false;
+    } else {
+        if (iter == l.rend() && !l.empty()) {
+            direction = RIGHT;
+            return true;
+        }
+        return false;
+    }
+}
+
+auto begin(list<int>& l, Direction direction = RIGHT) -> decltype((direction == RIGHT) ? l.begin() : l.rbegin()) {
+    if (RIGHT == direction) 
+        return l.begin();
+    else 
+        return l.rbegin();
+}
+
+auto end(list<int>& l, Direction direction = RIGHT) -> decltype((direction == RIGHT) ? l.end() : l.rend()){
+    if (RIGHT == direction) 
+        return l.end();
+    else 
+        return l.rend();
 }
 
 auto solution (list<int> input, int count) -> int {
     int ret= 0;
     int cur_info = 0;
     Direction direction = RIGHT;    
+
     while(cur_info != count) {
-        for(auto iter = input.begin(); iter != input.end();) {
+        auto iter = begin(input, direction);
+        auto end_iter = end(input, direction);
+        while(iter != end_iter) {
             if(*iter <= cur_info) {
-                ++ cur_info;
                 iter = input.erase(iter);
+                ++ cur_info;
             } else 
-                ++ iter;
+                iter = generalNext(iter, direction);
         }
-        if(!input.empty()) 
+        if (switchDirection(iter, input, direction)) {
+            if (RIGHT == direction)
+                direction = LEFT;
+            else
+                direction = RIGHT;
             ++ ret;
+        }
     }
     return ret;
 }
