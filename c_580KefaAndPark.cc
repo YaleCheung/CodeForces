d93f031ca54ad39470ee53ac70f1418c67a22660
YaleCheung <yalecheung@outlook.com>
2018-01-08 22:00:08 +0800

    add 580c

---
+++ c_580KefaAndPark.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using std::map;
+using std::vector;
+using std::cin;
+using std::cout;
+
+vector<int> visit;
+vector<bool> hasCat;
+map<int, 
+auto dfs(int vertex, int past_cat_num) {
+    visit
+}
